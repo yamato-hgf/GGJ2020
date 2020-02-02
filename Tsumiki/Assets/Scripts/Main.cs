eafdbce293f69ad6fa81103a7cd1b05d7c6d9556
ogawa minoru <gm.lrhv@gmail.com>
2020-02-02 13:30:51 +0900

    別ゲー

---
+++ Tsumiki/Assets/Scripts/Main.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] int _nextStage;
+    [SerializeField] Text _text;
+    [SerializeField] AudioClip[] _sounds;
+    [SerializeField] AudioSource _source;
+
+    Obj[] _objs = null;
+
+    Obj _clicked = null;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        _objs = FindObjectsOfType<Obj>();
+
+        yield return new WaitForSeconds(2f);
+        _source.clip = _sounds[0];
+        _source.Play();
+
+        foreach (var obj in _objs)
+        {
+            obj.Bomb();
+        }
+
+        yield return new WaitForSeconds(2f);
+        _text.text = "";
+
+        bool loop = true;
+        while (loop) {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _clicked = null;
+
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+                if (hit2d)
+                {
+                    _clicked = hit2d.transform.gameObject.GetComponent<Obj>();
+                }
+            }
+
+            if(_clicked != null)
+            {
+                var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                pos.z = 0;
+                _clicked.transform.position = pos;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                if(_clicked != null)
+                {
+                    _clicked = null;
+                }
+            }
+
+            loop = false;
+            foreach (var obj in _objs)
+            {
+                if (!obj.IsOK())
+                {
+                    loop = true;
+                    break;
+                }
+            }
+            yield return null;
+        }
+
+        _text.text = "Clear!!\nLet's Go to Next Stage";
+        _source.clip = _sounds[1];
+        _source.Play();
+        yield return new WaitForSeconds(3f);
+
+        while(true)
+        {
+            if(Input.anyKeyDown)
+            {
+                Application.LoadLevelAsync(_nextStage);
+            }
+            yield return null;
+        }
+    }
+}
---
+++ Tsumiki/Assets/Scripts/Obj.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obj : MonoBehaviour
+{
+    SpriteRenderer _sprite;
+    Vector3 _firstPosition;
+
+    public bool IsOK ()
+    {
+        if (Vector3.Distance(transform.position, _firstPosition) < 0.5f)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void Bomb()
+    {
+        _rigidbody.AddForce(Quaternion.Euler(0, 0, 360f * Random.value) * Vector2.up * 2500f);
+        _rigidbody.AddTorque(2500f * Random.value);
+    }
+
+    Rigidbody2D _rigidbody = null;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _firstPosition = transform.position;
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var color = _sprite.color;
+        color.a = IsOK() ? 1f : 0.5f;
+        _sprite.color = color;
+    }
+}
